2c1596e488fdb1f592b5bff9e8a49b08ee1e8f18
Orion <public4orion@163.com>
2024-08-07 19:54:48 +0800

    :sparkles: file validater

---
+++ validate.py
@@ -0,0 +1,18 @@
+from utils import is_valid, list_jsonl_files, is_subset, read_index
+from tqdm import tqdm
+import sys
+
+if __name__ == "__main__":
+    file_paths = list_jsonl_files("data/")
+    print(f"File paths (first 10): {file_paths[:10]}")
+    indexed_files = read_index("index.parquet")
+    print(f"Indexed files (first 10): {indexed_files[:10]}")
+    if not is_subset(indexed_files, file_paths):
+        print("No increasement on data!")
+        sys.exit(1)
+
+    for file in tqdm(file_paths):
+        if not is_valid(file):
+            print("Invalid file!")
+            sys.exit(1)
+    print("File valid!")
