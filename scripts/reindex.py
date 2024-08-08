67070c9bb0ace600a3230edcb65ddc77825fae00
Orion <public4orion@163.com>
2024-08-08 15:48:17 +0800

    :recycle:

--- scripts/reindex.py
+++ scripts/reindex.py
@@ -1,7 +1,11 @@
 from utils import list_files, update_index
 
 
-if __name__ == "__main__":
+def reindex(dir="data/", indexer="index.parquet"):
     file_paths = list_files("data/")
     update_index(file_paths, "index.parquet")
     print(f"reindexed to {file_paths}")
+
+
+if __name__ == "__main__":
+    reindex()
